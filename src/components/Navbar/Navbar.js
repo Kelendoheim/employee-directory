dc4a44a03537dfbc590ca54819eabb7f83279964
TDWalsh <thomas.walsh736@gmail.com>
2020-10-31 20:18:06 -0400

    starting to hardcode app

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Router, Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <p className="text-center">
+              Employee Directory: Doxx your employees!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
